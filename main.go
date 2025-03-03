7e1b55b9368469ac35d88b0d471e35319ecf9c15
GalihDJ <galihdj.biz@gmail.com>
2025-03-03 22:29:39 +0700

    Add template for main.go

--- main.go
+++ main.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
+)
+
+// @title           Amartha Loan Service
+// @version			0.0.1
+// @description     Swagger documentation for Amartha Loan Service API
+
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
+
+type Object struct {
+	Str string
+	Num int
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", os.Getenv("ACCESS_CONTROL_ALLOW_ORIGIN")) // Replace "*" with your frontend origin if needed
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func main() {
+	fmt.Println(fmt.Sprintf("Welcome to Amartha Loan Service!"))
+
+	router := gin.Default()
+
+	// Apply the CORS middleware
+	router.Use(CORSMiddleware())
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", "3000"), router); err != nil {
+		log.Fatal(err)
+	}
+}
